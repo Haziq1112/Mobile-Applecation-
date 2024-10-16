8ff089fbcc9a39f43435b2ac98f36d01485aea93
Haziq1112 <haziqahmad8000@gmail.com>
2024-10-16 10:47:54 +0500

    new lab in lab6

--- lab6/flutter_application_1/lib/main.dart
+++ lab6/flutter_application_1/lib/main.dart
@@ -2,5 +2,31 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(const MaterialApp(home:Text("Hey!!! Haziq Ahmad")));
+  runApp(
+  MaterialApp(
+  home:Scaffold(
+  body : Container(
+  decoration :const BoxDecoration (
+  gradient : LinearGradient (
+  begin : Alignment.topLeft,
+  end : Alignment.bottomRight,
+  colors:[
+  colors.blue,
+  colour.green,
+  ],
+  ),
+  ),
+  child : const Center(
+  child:text(
+  'Haziq Ahmad',
+  style:textStyle(
+  fontSize:24,
+  color:colors.white,
+  ),
+  ),
+  ),
+  ),
+  ),
+  ),
+  );
 }
